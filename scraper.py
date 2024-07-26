1d75d05700ec2755e431d076c94de48f190ae0c0
unknown <naman99sha@gmail.com>
2024-07-26 12:32:42 +0530

    Modified fetch_page function to perform retries

--- scraper.py
+++ scraper.py
@@ -5,18 +5,26 @@ import time
 
 class Scraper:
     #constructor to initialize url and other information
-    def __init__(self, pages_limit=5, proxy=None):
+    def __init__(self, pages_limit=5, proxy=None, max_retries=3, retry_delays=5):
         self.base_url = "https://dentalstall.com/shop/"
         self.pages_limit = pages_limit
         self.proxy = proxy
         self.proxies = {"http": proxy, "https": proxy} if proxy else None
+        self.max_retries = max_retries
+        self.retry_delays = retry_delays
 
     #fetch page function
     def fetch_page(self, url):
-        try:
-            response = requests.get(url, proxies=self.proxies)
-            response.raise_for_status()
-            return response.text
-        except requests.exceptions.RequestException as e:
-            print(f"Error while fetching {url} - {e}")
-            return
+        for attempt in range(self.max_retries):
+            try:
+                response = requests.get(url, proxies=self.proxies)
+                response.raise_for_status()
+                return response.text
+            except requests.exceptions.RequestException as e:
+                print(f"Error while fetching {url} - {e}")
+                if attempt < self.max_retries-1:
+                    print(f"Retrying in {self.retry_delays} seconds...")
+                    time.sleep(self.retry_delay)
+                else:
+                    print(f"Failed to fetch {url} after {self.max_retries} attempts.")
+                    return None
